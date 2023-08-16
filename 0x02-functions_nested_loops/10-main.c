7feb21f5443ea92a2faee6ed847f9b4b25a2bb9e
M-Kendrick <miracleamajama14@gmail.com>
2023-08-16 13:43:42 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-main.c
+++ 0x02-functions_nested_loops/10-main.c
@@ -1,11 +1,16 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - check the code
+ *
  * Return: Always 0.
  */
 int main(void)
 {
-	times_table();
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
 	return (0);
 }
