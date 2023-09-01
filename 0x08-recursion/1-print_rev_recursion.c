63df4ed8cf7acea7a68e1100e6bb196b1fa456cf
M-Kendrick <miracleamajama14@gmail.com>
2023-08-31 21:42:30 -0700

    1-print_rev_recursion.c by Miracle

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _print_rev_recursion -  a function that prints a string in reverse
+ * @s: the string to be printed
+ * Return: always 0
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
