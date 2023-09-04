009db3d7d9b063b57215d0acbd02206dde2f4515
M-Kendrick <miracleamajama14@gmail.com>
2023-09-04 08:15:19 -0700

    0x0B-malloc_free code by Miracle

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -38,8 +38,6 @@ char *str_concat(char *s1, char *s2)
 
 	conct = malloc(sizeof(char) * (i + ci + 1));
 
-
-
 	if (conct == NULL)
 
 		return (NULL);
@@ -47,28 +45,16 @@ char *str_concat(char *s1, char *s2)
 	i = ci = 0;
 
 	while (s1[i] != '\0')
-
 	{
-
 		conct[i] = s1[i];
-
 		i++;
-
 	}
-
-
 
 	while (s2[ci] != '\0')
-
 	{
-
 		conct[i] = s2[ci];
-
 		i++, ci++;
-
 	}
-
 	conct[i] = '\0';
-
 	return (conct);
 }
