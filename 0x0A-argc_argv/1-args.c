36090b8a9280901e6e02d07af987a5c6ccb16133
M-kendrick <miracleamajama14@gmail.com>
2023-11-07 01:45:55 -0800

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,8 +2,8 @@
 
 /**
  * main - prints number of arguments
- * @argc - argument count
- * @argv - argument vector
+ * @argc: argument count
+ * @argv: argument vector
  * Return: 0
  */
 
@@ -11,6 +11,6 @@ int main(int argc, char *argv[])
 {
 	(void) argv;
 
-	printf("%d\n", argc -1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
