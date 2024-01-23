823319f9a05056d3b35c777cfe52894428344ef9
M-kendrick <miracleamajama14@gmail.com>
2024-01-22 21:17:57 -0800

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - print dog
+ * @d: dog to be printed
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+		printf("Age: %f\n", (d->age) ? d->age : 0);
+		printf("Owner %s\n", (d->owner) ? d->owner : "(nil)");
+	}
+}
