cd755953a5efb156ea137ee79c00809795f52779
M-kendrick <miracleamajama14@gmail.com>
2024-01-22 20:39:31 -0800

    0x0E-structures_typedef

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
+/* more headers goes there */
 #include <stdio.h>
-/**
- * main - Entry point
- * Return: Always 0 (Success)
- */
+
+/* betty style doc for function main goes there */
+main - main function entry point
 
 int main(void)
 {
@@ -12,11 +12,10 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
 	if (n > 0)
-		printf("%d is positive\n", n);
+		printf("%d  is positive", n);
 	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
-		printf("%d is negative\n", n);
+		printf(
 	return (0);
 }
--- 0x01-variables_if_else_while/README.md
+++
@@ -1,2 +0,0 @@
-#This readme.md is for the projects under the alx-low_level_programming repository
-Specifically for the '0x01. C - Variables, if, else, while' task.
