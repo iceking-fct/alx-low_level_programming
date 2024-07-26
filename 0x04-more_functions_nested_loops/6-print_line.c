3e23febc42fd29e54604ad952273a8e67b59338c
iceking-fct <miracleamajama14@gmail.com>
2024-07-26 07:03:08 -0700

    6-print_line.c task resolved

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,13 +8,13 @@
 void print_line(int n)
 {
 	if (n <= 0)
-		_putchar('\n');
+		putchar('\n');
 	else
 	{
 		int i;
 
 		for (i = 1; i <= n; i++)
-			_putchar('_');
-		_putchar('\n');
+			putchar('_');
+		putchar('\n');
 	}
 }
